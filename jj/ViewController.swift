c168b7b9702cf9938997363d39ead84578d7eba8
C0deWave <jamg123123@naver.com>
2020-07-26 22:40:18 +0900

    Initial Commit

---
+++ jj/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  jj
+//
+//  Created by 장주명 on 2020/07/26.
+//  Copyright © 2020 C0deWave. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
