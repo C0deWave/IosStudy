57106a0da0e8cc84b7b21963a192e759c7681c49
C0deWave <jamg123123@naver.com>
2020-07-27 15:40:37 +0900

    나의 첫번째 IOS 앱입니다.

--- jj/ViewController.swift
+++ jj/ViewController.swift
@@ -9,12 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var helloIos: UILabel!
+    @IBOutlet weak var nameText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    //버튼에 대한 액션함수의 추가
+    @IBAction func btnSend(_ sender: Any) {
+        helloIos.text = "Hello" + nameText.text!
+    }
 }
 
